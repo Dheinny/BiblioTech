574e07c147ce3664585ace7fc5e33f80a95502c5
dheinny <dheinny@gmail.com>
2026-02-02 16:06:15 -0300

    feat: implement book entitiy and jpa repository

---
+++ src/main/java/com/fenix/bibliotech/domain/Book.java
@@ -0,0 +1,34 @@
+package com.fenix.bibliotech.domain;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank(message =  "{book.title.required}")
+    private String title;
+
+    @NotBlank(message = "{book.author.required}")
+    private String author;
+
+    @Column(unique = true)
+    private String isbn;
+
+    private Integer pages;
+
+    @Column(columnDefinition = "TEXT")
+    private String summary;
+}
---
+++ src/main/java/com/fenix/bibliotech/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.fenix.bibliotech.repository;
+
+import com.fenix.bibliotech.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BookRepository extends JpaRepository<Book, UUID> {
+
+    boolean existsByIsbn(String isbn);
+
+}
