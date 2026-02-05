9123d4075f3aedbabaf374b860d13d1f7a568813
dheinny <dheinny@gmail.com>
2026-02-05 13:34:27 -0300

    fix(catalog): adjust POST /books to return 201 Created with Location header

--- src/main/java/com/fenix/bibliotech/controller/BookController.java
+++ src/main/java/com/fenix/bibliotech/controller/BookController.java
@@ -1,15 +1,15 @@
 package com.fenix.bibliotech.controller;
 
-import com.fenix.bibliotech.domain.Book;
 import com.fenix.bibliotech.dto.request.BookRequestDTO;
 import com.fenix.bibliotech.dto.response.BookResponseDTO;
 import com.fenix.bibliotech.service.BookService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,7 +23,14 @@ public class BookController {
     @PostMapping
     public ResponseEntity<BookResponseDTO> create(@RequestBody @Valid BookRequestDTO bookDTO) {
         BookResponseDTO savedBook = bookService.save(bookDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedBook);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedBook.id())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(savedBook);
     }
 
     @GetMapping
--- src/main/java/com/fenix/bibliotech/dto/request/BookRequestDTO.java
+++ src/main/java/com/fenix/bibliotech/dto/request/BookRequestDTO.java
@@ -3,7 +3,9 @@ package com.fenix.bibliotech.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Builder;
 
+@Builder
 public record BookRequestDTO (
     @NotBlank(message = "{book.title.required}")
     String title,
