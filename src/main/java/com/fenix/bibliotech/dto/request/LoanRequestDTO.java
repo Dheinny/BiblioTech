554a8efda20c1fb765a7db938313ed1be95629fe
dheinny <dheinny@gmail.com>
2026-02-10 18:03:34 -0300

    feat(dto): create loan request/response records and implement manual mapper

--- src/main/java/com/fenix/bibliotech/dto/request/LoanRequestDTO.java
+++ src/main/java/com/fenix/bibliotech/dto/request/LoanRequestDTO.java
@@ -1,12 +1,14 @@
 package com.fenix.bibliotech.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.util.UUID;
 
 @Builder
-public record LoanDTO(
+public record LoanRequestDTO(
+        @NotNull(message = "{book.id.required}")
         UUID bookId,
         @NotBlank(message = "{customer.identification.required}")
         String customerName
