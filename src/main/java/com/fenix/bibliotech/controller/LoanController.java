81b316ce7dc791d79c02ba2fc018353b40ecae9d
dheinny <dheinny@gmail.com>
2026-02-25 20:06:11 -0300

    feat: implement book checkout endpoint with location header

---
+++ src/main/java/com/fenix/bibliotech/controller/LoanController.java
@@ -0,0 +1,34 @@
+package com.fenix.bibliotech.controller;
+
+import com.fenix.bibliotech.dto.request.LoanRequestDTO;
+import com.fenix.bibliotech.dto.response.LoanResponseDTO;
+import com.fenix.bibliotech.service.LoanService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/loans")
+public class LoanController {
+
+    private final LoanService loanService;
+
+    @PostMapping
+    public ResponseEntity<LoanResponseDTO> checkout (@Valid @RequestBody LoanRequestDTO loanRequest) {
+        LoanResponseDTO loanResponse = loanService.checkoutBook(loanRequest);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}").buildAndExpand(loanResponse.id())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(loanResponse);
+    }
+}
+
