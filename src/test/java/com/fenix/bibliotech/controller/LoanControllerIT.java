f007fc28046c1f588f5ad3fec8661611b346cf37
dheinny <dheinny@gmail.com>
2026-03-03 18:01:17 -0300

    test(loan): implement integration test for checkout success scenario
    -Add LoanControllerIT with MockMvc and repository state validation.
    -Create LoanIntegrationFactory to support complex IT scenarios.
    -Standardize assertions with AssertJ fluent API.

---
+++ src/test/java/com/fenix/bibliotech/controller/LoanControllerIT.java
@@ -0,0 +1,87 @@
+package com.fenix.bibliotech.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fenix.bibliotech.domain.helper.CustomerIdentifier;
+import com.fenix.bibliotech.domain.model.Book;
+import com.fenix.bibliotech.domain.model.BookLicense;
+import com.fenix.bibliotech.domain.model.Loan;
+import com.fenix.bibliotech.dto.request.LoanRequestDTO;
+import com.fenix.bibliotech.factory.LoanIntegrationFactory;
+import com.fenix.bibliotech.repository.LoanRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+@Import(LoanIntegrationFactory.class)
+public class LoanControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private LoanIntegrationFactory loanFactory;
+
+    @Autowired
+    private LoanRepository loanRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void shouldReturn201WhenCreateLoanWithSuccess() throws Exception {
+        // GIVEN
+        Book book = loanFactory.createBookBase();
+        BookLicense bkLicense = loanFactory.createBookLicense(book);
+        String customerName = "Nome1";
+        LoanRequestDTO loanRequest = LoanRequestDTO.builder()
+                .bookId(book.getId())
+                .customerName(customerName)
+                .build();
+        UUID customerIdExpected = CustomerIdentifier.generateId(customerName);
+
+        // WHEN & THEN
+        mockMvc.perform(post("/api/v1/loans")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loanRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(header().string("Location", containsString("/api/v1/loans/")))
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.customerId").value(customerIdExpected.toString()))
+                .andExpect(jsonPath("$.customerName").value(customerName))
+                .andExpect(jsonPath("$.bookTitle").value(book.getTitle()))
+                .andExpect(jsonPath("$.licenseCode").value(bkLicense.getLicenseCode()))
+                .andExpect(jsonPath("$.loanDate").isNotEmpty())
+                .andExpect(jsonPath("$.dueDate").isNotEmpty());
+
+        List<Loan> loan = loanRepository.findAll();
+
+        assertThat(loan).hasSize(1).first()
+                .satisfies(l -> {
+                    assertThat(l.getBookLicense())
+                            .hasFieldOrPropertyWithValue("licenseCode", bkLicense.getLicenseCode())
+                            .extracting(BookLicense::getBook)
+                            .hasFieldOrPropertyWithValue("title", book.getTitle());
+                    assertThat(l.getDueDate()).isAfter(l.getLoanDate());
+                    assertThat(l.getReturnDate()).isNull();
+                });
+    }
+}
---
+++ src/test/java/com/fenix/bibliotech/factory/LoanIntegrationFactory.java
@@ -0,0 +1,39 @@
+package com.fenix.bibliotech.factory;
+
+import com.fenix.bibliotech.domain.model.Book;
+import com.fenix.bibliotech.domain.model.BookLicense;
+import com.fenix.bibliotech.repository.BookLicenseRepository;
+import com.fenix.bibliotech.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.test.context.TestComponent;
+
+@TestComponent
+@RequiredArgsConstructor
+public class LoanIntegrationFactory {
+
+    private final BookRepository bookRepository;
+    private final BookLicenseRepository bkLicenseRepo;
+
+    public Book createBookBase() {
+        Book book = Book.builder()
+                .title("Book1")
+                .author("Author1")
+                .isbn("123456")
+                .pages(145)
+                .build();
+
+        return bookRepository.save(book);
+    }
+
+    public BookLicense createBookLicense(Book book) {
+        BookLicense bkLicense = getBookLicense(book, "COD1");
+        return bkLicenseRepo.save(bkLicense);
+    }
+
+    private BookLicense getBookLicense(Book book, String licenseCode) {
+        return BookLicense.builder()
+                .licenseCode(licenseCode)
+                .book(book)
+                .build();
+    }
+}
